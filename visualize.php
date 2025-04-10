e623c3d47d0bced7fed65183415580645bc9b8d6
miguel salviano <miguelcf.tg@gmail.com>
2025-04-10 16:45:27 -0300

    feat: back button added

--- visualize.php
+++ visualize.php
@@ -2,11 +2,13 @@
 include_once 'templates/header.php';
 
 ?>
-
 <div class="container" id="view-fornecedor-container">
+    <div class="back-link-container">
+        <a href="index.php" class="btn btn-primary" id="back-link">Voltar</a>
+    </div>
     <h1 id="main-title"><?= $fornecedor['name'] ?></h1>
     <p class="bold">Telefone:</p>
     <p><?= $fornecedor['phone'] ?></p>
-    <p class="bold">Área da atuação:</p>
+    <p class="bold">Ramo de atividade:</p>
     <p><?= $fornecedor['activity'] ?></p>
 </div>
